03ee80ad300fa776938f24ad1fe9a48d5a01dd5c
Kevin Irwin <kevin.irwin@tpsgc-pwgsc.gc.ca>
2014-11-10 10:48:43 -0500

    Initial content

---
+++ Validator_GUI/open_data_tool.pl
@@ -0,0 +1,80 @@
+#!/usr/bin/perl -w
+#***********************************************************************
+#
+# Name:   open_data_tool.pl
+#
+# $Revision$
+# $URL$
+# $Date$
+#
+# Synopsis: open_data_tool.pl
+#
+# Description:
+#
+#   Run the wpss_tool.pl program in Open Data mode.
+#
+# Terms and Conditions of Use
+# 
+# Unless otherwise noted, this computer program source code
+# is covered under Crown Copyright, Government of Canada, and is 
+# distributed under the MIT License.
+# 
+# MIT License
+# 
+# Copyright (c) 2011 Government of Canada
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+# and/or sell copies of the Software, and to permit persons to whom the 
+# Software is furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR 
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
+# OTHER DEALINGS IN THE SOFTWARE.
+# 
+#***********************************************************************
+
+use File::Basename;
+use strict;
+
+my (@paths, $this_path, $program_dir, $program_name, $paths, $rc);
+
+#
+# Get our program directory, where we find supporting files
+#
+$program_dir  = dirname($0);
+$program_name = basename($0);
+
+#
+# If directory is '.', search the PATH to see where we were found
+#
+if ( $program_dir eq "." ) {
+    $paths = $ENV{"PATH"};
+    @paths = split( /:/, $paths );
+
+    #
+    # Loop through path until we find ourselves
+    #
+    foreach $this_path (@paths) {
+        if ( -x "$this_path/$program_name" ) {
+            $program_dir = $this_path;
+            last;
+        }
+    }
+}
+
+#
+# Run the wpss_tool.pl program
+#
+chdir($program_dir);
+$rc = system(".\\wpss_tool.pl -open_data @ARGV");
+exit($rc);
