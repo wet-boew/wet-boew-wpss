3102b9b0347eeceed273ae74cec01a4b06f76538
irwink <kevin.irwin@tpsgc-pwgsc.gc.ca>
2022-09-06 10:01:29 -0400

    Check multiple locations for chrome
    Verify server by getting a test URL.

--- Crawler/crawler_puppeteer.pm
+++ Crawler/crawler_puppeteer.pm
@@ -2,9 +2,9 @@
 #
 # Name:   crawler_puppeteer.pm
 #
-# $Revision: 2193 $
+# $Revision: 2378 $
 # $URL: svn://10.36.148.185/WPSS_Tool/Crawler/Tools/crawler_puppeteer.pm $
-# $Date: 2021-10-29 11:04:59 -0400 (Fri, 29 Oct 2021) $
+# $Date: 2022-08-31 13:53:57 -0400 (Wed, 31 Aug 2022) $
 #
 # Description:
 #
@@ -60,6 +60,7 @@ use File::Path qw(remove_tree);
 use LWP::UserAgent;
 use File::Temp qw/ tempfile tempdir /;
 use IO::Socket::INET;
+use IO::Socket::SSL qw( SSL_VERIFY_NONE );
 
 my $have_threads = eval 'use threads; 1';
 if ( $have_threads ) {
@@ -98,10 +99,13 @@ my (@paths, $this_path, $program_dir, $program_name, $paths);
 my ($puppeteer_user_dir, $puppeteer_server_last_arg);
 my ($puppeteer_server_cmnd, $puppeteer_chrome_min_version);
 my ($debug) = 0;
-my ($markup_server_port) = "8000";
+my ($markup_server_port_start) = "8000";
+my ($markup_server_port) = $markup_server_port_start;
 my ($retry_page) = 0;
 my ($default_windows_chrome_path);
 my ($chrome_install_error_reported) = 0;
+my ($user_agent_name) = "Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) WPSS_Tool";
+my ($default_user_agent_check_url) = "https://www.google.com/";
 
 my ($headless_chrome_installed, $chrome_path, $chrome_install_error);
 my ($user_agent_software_versions);
@@ -300,7 +304,7 @@ sub Crawler_Puppeteer_Config {
         # Check for known configuration variables
         #
         if ( $key eq "markup_server_port" ) {
-            $markup_server_port = $value;
+            $markup_server_port_start = $value;
         }
         elsif ( $key eq "default_windows_chrome_path" ) {
             $default_windows_chrome_path = $value;
@@ -381,11 +385,11 @@ sub Check_Puppeteer_Requirements {
     #
     # Do we have configuration parameters?
     #
-    if ( (! defined($markup_server_port))          ||
+    if ( (! defined($markup_server_port_start))          ||
          (! defined($default_windows_chrome_path)) ||
          (! defined($puppeteer_chrome_min_version)) ) {
         print "No puppeteer configuration paramters supplied\n" if $debug;
-        print STDERR "No puppeteer configuration paramters supplied\n";
+        print STDERR "crawler_puppeteer: No puppeteer configuration paramters supplied\n";
         $meets_requirements = 0;
         $chrome_install_error = String_Value("No puppeteer configuration paramters supplied");
         return($meets_requirements);
@@ -403,8 +407,17 @@ sub Check_Puppeteer_Requirements {
         if ( $chrome_path =~ /Could not find/i ) {
             print "Chrome not in path, use default\n" if $debug;
             if ( defined($default_windows_chrome_path) ) {
-                print "Default chrome path = $default_windows_chrome_path\n" if $debug;
-                $chrome_path = $default_windows_chrome_path;
+                #
+                # Check each path for an instance of Chrome
+                #
+                print "Default chrome paths = $default_windows_chrome_path\n" if $debug;
+                foreach $file_path (split(/\n/, $default_windows_chrome_path)) {
+                    if ( -f $file_path ) {
+                        $chrome_path = $file_path;
+                        print "Chrome found at path = $file_path\n" if $debug;
+                        last;
+                    }
+                }
             }
             else {
                 print "No default chrome path\n" if $debug;
@@ -439,7 +452,7 @@ sub Check_Puppeteer_Requirements {
             # No chrome executable
             #
             print "Chrome executable not found\n" if $debug;
-            print STDERR "Chrome not installed, headless chrome not available\n";
+            print STDERR "crawler_puppeteer: Chrome not installed, headless chrome not available\n";
             $meets_requirements = 0;
             $chrome_install_error = String_Value("Chrome not installed, headless chrome not available");
         }
@@ -451,7 +464,7 @@ sub Check_Puppeteer_Requirements {
         $file_path = `where node 2>&1`;
         if ( $file_path =~ /Could not find/i ) {
             print "Node not in path\n" if $debug;
-            print STDERR "Node not installed, headless chrome not available\n";
+            print STDERR "crawler_puppeteer: Node not installed, headless chrome not available\n";
             $meets_requirements = 0;
             $chrome_install_error = String_Value("Node not installed, headless chrome not available");
         }
@@ -466,7 +479,7 @@ sub Check_Puppeteer_Requirements {
         #
         # Not Windows.
         #
-        print STDERR "Not Windows, headless chrome not available\n";
+        print STDERR "crawler_puppeteer: Not Windows, headless chrome not available\n";
         $meets_requirements = 0;
     }
     
@@ -486,7 +499,7 @@ sub Check_Puppeteer_Requirements {
             # Either no versions or version below minimum
             #
             print "Chrome version below minimum or no minimum value set\n" if $debug;
-            print STDERR "Headless Chrome version below minimum or no version found\n";
+            print STDERR "crawler_puppeteer: Headless Chrome version below minimum or no version found\n";
             $chrome_install_error = String_Value("Chrome version below minimum value");
             $meets_requirements = 0;
         }
@@ -548,7 +561,7 @@ node \"$js_filename\"
         print "Output = $node_output\n" if $debug;
         if ( $node_output =~ /Cannot find module/im ) {
             print "Puppeteer-core module not installed, output = $node_output\n" if $debug;
-            print STDERR "Node/Puppeteer-core module not installed, headless chrome not available\n";
+            print STDERR "crawler_puppeteer: Node/Puppeteer-core module not installed, headless chrome not available\n";
             $chrome_install_error = String_Value("Puppeteer-core module not installed");
             $meets_requirements = 0;
         }
@@ -578,7 +591,7 @@ node \"$js_filename\"
 #
 # Name: Crawler_Puppeteer_Start_Markup_Server
 #
-# Parameters: none
+# Parameters: user_agent_check_url - URL to check user agents
 #
 # Description:
 #
@@ -595,9 +608,10 @@ node \"$js_filename\"
 #
 #***********************************************************************
 sub Crawler_Puppeteer_Start_Markup_Server {
+    my ($user_agent_check_url) = @_;
 
-    my ($debug_option, $sec, $min, $hour, $time, $cmd, $rc);
-    my ($port, $port_found, $socket);
+    my ($debug_option, $sec, $min, $hour, $time, $cmd, $rc, $url);
+    my ($port, $port_found, $socket, $lwp_user_agent, $resp);
 
     #
     # Check to see if the required programs and files are
@@ -616,20 +630,55 @@ sub Crawler_Puppeteer_Start_Markup_Server {
         Crawler_Puppeteer_Clear_Cache("");
         
         #
-        # Check to see if port number is available
+        # Pass debug flag to markup server
         #
-        $port = $markup_server_port;
+        if ( $debug ) {
+            $debug_option = " -debug";
+        }
+        else {
+            $debug_option = "";
+        }
+
+        #
+        # Check to see that user data directory exists.  It might have
+        # been removed in a cache clearing.
+        #
+        if ( ! -d $puppeteer_user_dir ) {
+            print "Missing directory $puppeteer_user_dir\n" if $debug;
+            mkdir($puppeteer_user_dir);
+        }
+        
+        #
+        # Check for a user agent check url
+        #
+        if ( ! defined($user_agent_check_url) || ($user_agent_check_url eq "") ) {
+            $user_agent_check_url = $default_user_agent_check_url;
+        }
+
+        #
+        # Create a LWP::UserAgent object to test puppeteer
+        #
+        print "Create LWP::UserAgent user agent $user_agent_name\n" if $debug;
+        $lwp_user_agent = LWP::UserAgent->new(agent => "$user_agent_name");
+        $lwp_user_agent->ssl_opts(verify_hostname => 0,
+                                  SSL_verify_mode => SSL_VERIFY_NONE);
+        $lwp_user_agent->timeout("60");
+
+        #
+        # Look for a usable port for the markup server
+        #
+        $port = $markup_server_port_start;
         $port_found = 0;
         while ( ! $port_found ) {
             #
             # Create a socket object and attempt to bind to the port
             #
             print "Attempt to connect to port $port\n" if $debug;
-            $socket = new IO::Socket::INET (LocalHost => '127.0.0.1',
-                                            LocalPort => $port,
-                                            Proto => 'tcp',
-                                            Listen => 5,
-                                            Reuse => 1);
+            $socket = new IO::Socket::INET(LocalHost => '127.0.0.1',
+                                           LocalPort => $port,
+                                           Proto => 'tcp',
+                                           Listen => 5,
+                                           Reuse => 1);
         
             #
             # Were we successful?
@@ -639,65 +688,70 @@ sub Crawler_Puppeteer_Start_Markup_Server {
                 # Found a usable port, release it and stop search
                 #
                 $socket->close();
-                $port_found = 1;
+
+                #
+                # Start the puppeteer_markup_server.js program in Node
+                #
+                ($sec, $min, $hour) = (localtime)[0,1,2];
+                $time = sprintf("%02d:%02d:%02d", $hour, $min, $sec);
+                print "Crawler_Puppeteer_Start_Markup_Server at $time\n" if $debug;
+                $cmd = "$puppeteer_server_cmnd $port \"$chrome_path\" \"$puppeteer_user_dir\" $debug_option";
+                print "$cmd\n" if $debug;
+                $rc = system("$cmd");
+                print "Puppeteer server started, rc = $rc\n" if $debug;
+                sleep(5);
+
+                #
+                # Test the puppeteer markup server
+                #
+                $url = "http://127.0.0.1:$port/GET?url=$user_agent_check_url";
+                print "Test puppeteer markup server by getting $url\n" if $debug;
+                $resp = $lwp_user_agent->get($url);
+                if ( defined($resp) && ($resp->is_success) ) {
+                    #
+                    # Save the port number to be used in page requests
+                    #
+                    print "puppeteer markup server is running and responds\n" if $debug;
+                    $markup_server_port = $port;
+                    $port_found = 1;
+
+                    #
+                    # Set flag to indicate that the markup server has been started.
+                    #
+                    $markup_server_running = 1;
+                    return(1);
+                }
+                #
+                # test failed
+                #
+                else {
+                    print "Got error response from puppeteer markup server\n" if $debug;
+                    if ( defined($resp) ) {
+                        print "  HTTP response:" . $resp->status_line . "\n" if $debug;
+                        print $resp->as_string . "\n" if $debug;
+                    }
+                    else {
+                        print STDERR "  HTTP response: undefined\n" if $debug;
+                    }
+                }
             }
-            else {
-                #
-                # Could not connect to socket, increment port number.
-                # Try a maximum of 10 times.
-                #
+            
+            #
+            # If we didn't find a usable port, increment port number.
+            # Try a maximum of 10 times.
+            #
+            if ( ! $port_found ) {
                 if ( ($port - $markup_server_port) < 10 ) {
                     $port++;
                 }
                 else {
-                    print "Failed to find a free port starting at $markup_server_port\n" if $debug;
-                    print STDERR "Failed to find a free port for puppeteer starting at $markup_server_port\n";
+                    print "Failed to find a free port starting at $markup_server_port_start\n" if $debug;
+                    print STDERR "Failed to find a free port for puppeteer starting at $markup_server_port_start\n";
                     $markup_server_running = 0;
                     return(0);
                 }
             }
         }
-
-        #
-        # Pass debug flag to markup server
-        #
-        if ( $debug ) {
-            $debug_option = " -debug";
-        }
-        else {
-            $debug_option = "";
-        }
-        
-        #
-        # Check to see that user data directory exists.  It might have
-        # been removed in a cache clearing.
-        #
-        if ( ! -d $puppeteer_user_dir ) {
-            print "Missing directory $puppeteer_user_dir\n" if $debug;
-            mkdir($puppeteer_user_dir);
-        }
-    
-        #
-        # Start the puppeteer_markup_server.js program in Node
-        #
-        ($sec, $min, $hour) = (localtime)[0,1,2];
-        $time = sprintf("%02d:%02d:%02d", $hour, $min, $sec);
-        print "Crawler_Puppeteer_Start_Markup_Server at $time\n" if $debug;
-        $cmd = "$puppeteer_server_cmnd $port \"$chrome_path\" \"$puppeteer_user_dir\" $debug_option";
-        print "$cmd\n" if $debug;
-        $rc = system("$cmd");
-
-        #
-        # Set flag to indicate that the markup server has been started.
-        #
-        $markup_server_running = 1;
-        sleep(5);
-        
-        #
-        # Return server started
-        #
-        print "Puppeteer server started, rc = $rc\n" if $debug;
-        return(1);
     }
     
     #
@@ -738,8 +792,8 @@ sub Server_Page_Markup {
     #
     print "Server_Page_Markup\n" if $debug;
     if ( ! $markup_server_running ) {
-        print "Restart markup server\n" if $debug;
-        Crawler_Puppeteer_Start_Markup_Server();
+        print "Restart markup server, test url = $this_url\n" if $debug;
+        Crawler_Puppeteer_Start_Markup_Server($this_url);
     }
     
     #
@@ -827,16 +881,59 @@ sub Server_Page_Markup {
             print "Found page markup and load time $load_time\n" if $debug;
             $generated_markup{"generated_content"} = $content;
         }
+        #
+        # Check for error message from the markup server
+        #
+        elsif ( $output =~ /===== PAGE ERROR ENDS =====/ ) {
+            print "Found error end marker\n" if $debug;
+            $error = 0;
+            foreach $line (split(/\n/, $output)) {
+                if ( $line =~ /===== PAGE ERROR BEGINS =====/ ) {
+                    #
+                    # Start of error message
+                    #
+                    $error = 1;
+                }
+                elsif ( $line =~ /===== PAGE ERROR ENDS =====/ ) {
+                    #
+                    # End of error message
+                    #
+                    $error = 0;
+                }
+                elsif ( $error ) {
+                    $content .= $line . "\n";
+                }
+            }
+            print "Found page error marker\n" if $debug;
+            $generated_markup{"error"} = $content;
+            print "Error with puppeteer/headless browser output\n" if $debug;
+            print STDERR "crawler_puppeteer: Error Server_Page_Markup\n";
+            print STDERR "Response successful, page error marker found\n";
+            print STDERR "  get($url)\n";
+            print STDERR "$output\n";
+            
+            #
+            # Try stopping and restarting the markup server
+            #
+            print "Attempting a restart of the crawler_puppeteer server, test url = $this_url\n" if $debug;
+            Crawler_Puppeteer_Start_Markup_Server($this_url);
+        }
         else {
             #
             # Error running puppeteer
             #
             print "Error with puppeteer output\n" if $debug;
-            print STDERR "Error Server_Page_Markup\n";
+            print STDERR "crawler_puppeteer: Error Server_Page_Markup\n";
             print STDERR "Missing page markup markers\n";
             print STDERR "  get($url)\n";
             print STDERR "$output\n";
+            
             $generated_markup{"generated_content"} = "";
+            #
+            # Try stopping and restarting the markup server
+            #
+            print "Attempting a restart of the crawler_puppeteer server, test url = $this_url\n" if $debug;
+            Crawler_Puppeteer_Start_Markup_Server($this_url);
         }
     }
     #
@@ -869,25 +966,36 @@ sub Server_Page_Markup {
                     $content .= $line . "\n";
                 }
             }
-            print "Found page error\n" if $debug;
+            print "Found page error marker\n" if $debug;
             $generated_markup{"error"} = $content;
             print "Error with puppeteer/headless browser output\n" if $debug;
-            print STDERR "Error Server_Page_Markup\n";
-            print STDERR "Response not successful\n";
-            print STDERR "Missing page markup markers\n";
+            print STDERR "crawler_puppeteer: Error Server_Page_Markup\n";
+            print STDERR "Response not successful, page error marker found\n";
             print STDERR "  get($url)\n";
             print STDERR "$output\n";
+
+            #
+            # Try stopping and restarting the markup server
+            #
+            print "Attempting a restart of the crawler_puppeteer server, test url = $this_url\n" if $debug;
+            Crawler_Puppeteer_Start_Markup_Server($this_url);
         }
         else {
             #
             # Error running puppeteer
             #
             print "Error with puppeteer output\n" if $debug;
-            print STDERR "Error Server_Page_Markup\n";
+            print STDERR "crawler_puppeteer: Error Server_Page_Markup\n";
             print STDERR "Response not successful\n";
             print STDERR "  get($url)\n";
             print STDERR "$output\n";
             $generated_markup{"error"} = "";
+
+            #
+            # Try stopping and restarting the markup server
+            #
+            print "Attempting a restart of the crawler_puppeteer server, test url = $this_url\n" if $debug;
+            Crawler_Puppeteer_Start_Markup_Server($this_url);
         }
         $generated_markup{"generated_content"} = "";
     }
@@ -901,7 +1009,8 @@ sub Server_Page_Markup {
         #
         # Restart the markup server (it may have exited after an idle timeout)
         #
-        Crawler_Puppeteer_Start_Markup_Server();
+        print "Attempting a restart of the crawler_puppeteer server, test url = $this_url\n" if $debug;
+        Crawler_Puppeteer_Start_Markup_Server($this_url);
 
         #
         # Have we already tried to get this page for the 2nd time?
@@ -919,7 +1028,7 @@ sub Server_Page_Markup {
             #
             # Failed to get page twice, report error.
             #
-            print STDERR "Error in Server_Page_Markup\n";
+            print STDERR "crawler_puppeteer: Error in Server_Page_Markup\n";
             print STDERR "Response not defined\n";
             print STDERR "  get($url)\n";
             if ( defined($resp) ) {
